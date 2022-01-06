9be19a5cb9ed25c98e8193625edf696d89d1e351
jiwoo <hjw6112@gmail.com>
2022-01-06 23:20:53 +0900

    add Colors.js

---
+++ src/theme/Colors.js
@@ -0,0 +1,17 @@
+/* 주요 색상 정의 */
+const Colors = {
+    main : '#3561FF',
+    main2: '#FF3535',
+    black: '#202639',
+    white: '#FFFFFF',
+    gray: '#F2F4F7',
+    gray2: '#E5E5E5',
+    darkGray: '#ADADAD',
+    sub1: '#E5ECFF',
+    sub2: '#FFE8D4',
+    sub3: '#D2EADE',
+    sub4: '#EAE3FF',
+    sub5: '#FFDDDD',
+};
+
+export default Colors;
