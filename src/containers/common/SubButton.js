8338bd554dbf56288c1abbe15973946ea6b74926
jiwoo <hjw6112@gmail.com>
2022-01-14 13:03:44 +0900

    rename TextButton to SubButton

---
+++ src/containers/common/SubButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import colors from '../../theme/Colors';
+import { type } from '../../theme/Fonts';
+
+function SubButton({ text, doFunction }) {
+
+    return (
+        <View style={styles.container}
+            onTouchEnd={() => { doFunction() }}>
+            <Text style={ styles.font1}>{text}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 32,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.sub1,
+        borderRadius: 10,
+        paddingHorizontal: 18,
+    },
+    font1: {
+        fontSize: 12,
+        color: colors.main,
+        fontFamily: type.Roboto_Bold,
+    },
+});
+
+export default SubButton;
